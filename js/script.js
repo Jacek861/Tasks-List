37ca5e07b257194fcb1f3eeb7ae4f15454934d0e
Jacek Opalinski <jacek.opalinski861@gmail.com>
2023-02-06 12:45:19 +0000

    function of task remove added to js

--- js/script.js
+++ js/script.js
@@ -13,21 +13,33 @@
         render();
     };
 
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
     const render = () => {
         htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
             <li class="tasks__list">
-            <button class="task__button" >${task.done ? "✓" : ""}</button>
+            <button class="task__button">${task.done ? "✓" : ""}</button>
             <span class="task__content">${task.content}</span>
-            <button class="task__button task__button--remove">🗑</button>
+            <button class="task__button task__button--remove js-remove">🗑</button>
             </li>`;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    }
 
+        const removeButtons = document.querySelectorAll(".js-remove")
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+               removeTask(index)
+            });
+        });
+
+    }
 
 
     const onFormSubmit = (event) => {
@@ -38,9 +50,7 @@
         if (newTaskContent === "") {
             return;
         }
-
         addNewTask(newTaskContent);
-
     }
 
     const init = () => {
